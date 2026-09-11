f34f9f6d05b488071ab89124ee4d3ef6aeebb7da
samake-2T2 <jithub@naver.com>
2026-09-11 13:35:27 +0900

    Add GitHub Actions firmware build workflow and include librdrcommon.a

--- builddefs/common_features.mk
+++ builddefs/common_features.mk
@@ -731,6 +731,7 @@ ifeq ($(strip $(LIB8TION_ENABLE)), yes)
 endif
 
 # SRC += $(LIB_PATH)/rdr_lib/rdr_common.c
+COMMON_VPATH += $(LIB_PATH)/rdr_lib
 LIB_NAME = rdrcommon
 LIBRARY = $(LIB_PATH)/rdr_lib/lib$(LIB_NAME).a
 LDFLAGS += -L$(LIB_PATH)/rdr_lib -l$(LIB_NAME)
